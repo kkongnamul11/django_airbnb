83ccae3d51a95e843bbb8a1d3116372702c662a0
geonuk <kgu0724@makeitnow.kr>
2022-05-12 22:37:10 +0900

    django rest framework FINISH

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,7 +5,8 @@ from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer()
+    user = UserSerializer(read_only=True)
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -22,6 +23,20 @@ class RoomSerializer(serializers.ModelSerializer):
                 if check_in == check_out:
                     raise serializers.ValidationError("Not enough time between changes")
             return data
+
+
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        room = Room.objects.create(**validated_data, user=request.user)
+        return room
 
 
 # class WriteRoomSerializer(serializers.Serializer):
